50eace04181fd399cb7a1184e265eb814ef3f1fb
Athlie54 <kjself04@gmail.com>
2025-04-18 04:32:05 -0500

    input graph list

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -7,35 +8,45 @@ public class Main {
     public static void main(String[] args) {
         int qty;
         Heap<Edge> heap;
-        Edge[] connections;
+        ArrayList<ArrayList<Edge>> connections = null;
 
         File f = new File("src/main/resources/graph.txt").getAbsoluteFile();
         //print true if f exists
         //System.out.println(f.exists());
         try {
-            //File f = new File("graph.txt");
             Scanner reader = new Scanner(f);
-            //System.out.print("we here\n");
+
             if (reader.hasNextLine()) {
                 qty = Integer.parseInt(reader.nextLine()); //number of vertices
-                connections = new Edge[qty];
-                //System.out.println(Edge qty + "\n");
+                connections = new ArrayList<ArrayList<Edge>>();
+                for (int i = 0; i < qty; i++) {
+                    connections.add(new ArrayList<Edge>());
+                }
 
-                //Integer[] array = new Integer[3];
                 //once you've acquired the number of vertices
                 while (reader.hasNextLine()) {
-                    for (int i=0; i<qty; i++) {
-                        if (!reader.hasNextLine()) break; // avoid crash
-                        String[] array = reader.nextLine().split(" ");
-                        System.out.println(Arrays.toString(array));
-                        int src = Integer.parseInt(array[0]);
-                        int dest = Integer.parseInt(array[1]);
-                        float weight = Float.parseFloat(array[2]);
-                        Edge e = new Edge(src, dest, weight);
-                        connections[i] = e;
-                        //System.out.println(e);
-                    }
+                    if (!reader.hasNextLine()) break; // avoid crash
+                    String[] array = reader.nextLine().split(" ");
+                    //System.out.println(Arrays.toString(array));
+                    int src = Integer.parseInt(array[0]);
+                    int dest = Integer.parseInt(array[1]);
+                    float weight = Float.parseFloat(array[2]);
+                    Edge e = new Edge(src, dest, weight);
+                    connections.get(src - 1).add(e);
+                    e = new Edge(dest, src, weight);
+                    connections.get(dest - 1).add(e);
+
+                    //System.out.println(e);
                 }
+            }
+
+            System.out.println("The input graph is represented in an adjacent list as: ");
+            for(int k=0;k < connections.size(); k++) {
+                System.out.print(k+1);
+                for(Edge e: connections.get(k)) {
+                    System.out.print(" --> (" + e.destination + ", " + e.weight + ")");
+                }
+                System.out.println("\n");
             }
 
             //heap = new Heap<Edge>(connections,qty);
