c362ee82dfbd77cc0164c0159ee052736eb18532
evan52703 <evantrejo527@gmail.com>
2025-04-18 11:38:46 -0500

    implemented Prim's Algorithm and MST printing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int qty;
-        Heap<Edge> heap;
+        //Heap<Edge> heap;
         ArrayList<ArrayList<Edge>> connections = null;
 
         File f = new File("src/main/resources/graph.txt").getAbsoluteFile();
@@ -45,6 +44,54 @@ public class Main {
             for(int k=0;k < connections.size(); k++) {
                 adjacentList(connections.get(k),k+1);
             }
+
+            //Prim's Alg
+            //@author Evan Trejo
+            int numVerticies = connections.size();
+            Float[] key = new Float[numVerticies];
+            int[] parent = new int[numVerticies];
+
+            for (int i = 0; i < numVerticies; i++){
+                key[i] = Float.MAX_VALUE; //infinity
+                parent[i] = -1;
+            }
+            key[0] = 0f; //Index 0
+
+            Heap<Float> heap = new Heap<>(key, numVerticies);
+            
+            System.out.println("\nRunning Prim algorithm");
+            while (true) {
+                int u = heap.min_id();
+                if (u == -1) break;
+            
+                heap.delete_min();
+            
+                for (Edge edge : connections.get(u)) {
+                    int v = edge.destination - 1; //zero-based
+                    float weight = edge.weight;
+            
+                    if (heap.in_heap(v) && weight < key[v]) {
+                        key[v] = weight;        //update key array
+                        parent[v] = u;
+                        heap.decrease_key(v, weight);
+                    }
+                }
+            }
+            
+            //Print MST
+            //@author Evan Trejo
+            System.out.println("DONE");
+            System.out.println("\nMinimum Spanning Tree:");
+            float totalWeight = 0;
+            for (int i = 1; i < numVerticies; i++) {
+                if (parent[i] != -1) {
+                    float weight = key[i];
+                    totalWeight += weight;
+                    System.out.printf("(%d, %d) weight: %.2f\n", parent[i] + 1, i + 1, weight);
+                }
+            }
+            System.out.printf("\n");
+            //System.out.printf("Total weight of MST: %.2f\n\n", totalWeight);
 
             reader.close();
         } catch (FileNotFoundException e) {
